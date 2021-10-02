0caeea3882b63b6826b30b74042df3bbaace7a63
anjirajardula <anjirajardula@gmail.com>
2021-10-02 10:32:54 +0530

    Added: Check the Employee Present Or Not

--- src/main/java/com/bridgelabz/EmployeeWageOps.java
+++ src/main/java/com/bridgelabz/EmployeeWageOps.java
@@ -1,7 +1,24 @@
 package com.bridgelabz;
 
+/**
+ * Employee Wage Program By Using ops Concept
+ *
+ * @author : Anji Raj Ardula
+ * @version : 16.0
+ * @since : 02/10/2021
+ */
+
 public class EmployeeWageOps {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program By using Ops concept");
+        int IS_PRESENT = 1;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_PRESENT)
+            System.out.println(" Employee is Present");
+        else
+            System.out.println(" Employee is Absent");
     }
 }
+
+
