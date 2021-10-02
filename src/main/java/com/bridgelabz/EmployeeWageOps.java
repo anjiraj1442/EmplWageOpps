7795f6d0c157d6290f7d390ea1edf943b17a2a83
anjirajardula <anjirajardula@gmail.com>
2021-10-02 10:10:47 +0530

    Added: Welcome to Employee Wage Program

---
+++ src/main/java/com/bridgelabz/EmployeeWageOps.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeeWageOps {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Program By using Ops concept");
+    }
+}
